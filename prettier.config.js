7ebc636c28431cecd786d2a47459e4e1daba1065
saira512dev <512saira@gmail.com>
2025-05-17 15:00:59 +0530

    tailwind config set

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js (ESM-compatible)
+import prettierPluginTailwindcss from "prettier-plugin-tailwindcss";
+
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: [prettierPluginTailwindcss],
+};
+
+export default config;
