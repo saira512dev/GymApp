2eb225760e11591da5a6fbad477b80c58702b06c
saira512dev <512saira@gmail.com>
2025-05-17 15:01:34 +0530

    started with the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Navbar from "@/scenes/Navbar";
+import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
-  return <div>Hello</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
+  return (
+    <div className="bg-gray-50">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
